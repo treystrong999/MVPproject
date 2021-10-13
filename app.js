7f3543be34e710daaee9e257948f4467d1bbf781
Jesse Strong <jessestrong99@Jesses-Air.attlocal.net>
2021-10-13 18:53:36 -0500

    trying to fix css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+const path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
